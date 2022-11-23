52cbc7dbbfe2c54fb4857e435a764dc11e32e428
gagdiez <gagdiez.c@gmail.com>
2022-11-23 11:45:36 -0300

    Update lib.rs

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -4,7 +4,7 @@ use near_sdk::{near_bindgen, Balance, Gas};
 mod deploy;
 mod manager;
 
-const NEAR_PER_STORAGE: Balance = 10_000_000_000_000_000_000; // 10e19yⓃ
+const NEAR_PER_STORAGE: Balance = 10_000_000_000_000_000_000; // 10e18yⓃ
 const DEFAULT_CONTRACT: &[u8] = include_bytes!("./donation-contract/donation.wasm");
 const TGAS: Gas = Gas(10u64.pow(12)); // 10e12yⓃ
 const NO_DEPOSIT: Balance = 0; // 0yⓃ
